9ead2ce46cf9687d96d9fa0f327c0433bb49829c
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2022-10-16 21:07:40 -0300

    Agregar archivos de proyecto.

---
+++ Sortedset-Hashset/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortedset_Hashset
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //string frase;
+            //SortedSet<string> Diccionario = new SortedSet<string>();
+
+            //Console.WriteLine("Ingrese frase al alzar. Si ya no desea ingresar mas frases, no escriba nada y de ENTER.");
+            //frase = Console.ReadLine();
+            //Diccionario.Add(frase);
+
+            //while (frase != "")
+            //{
+            //    frase = Console.ReadLine();
+
+            //    Diccionario.Add(frase);
+
+            //    if (Diccionario.Contains(frase))
+            //    {
+            //        Console.WriteLine("La frase ingresada, fue escrita anteriormente");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("La frase ingresada, NO fue escrita anteriormente");
+            //    }
+
+            //}
+
+            //if (frase == "")
+            //{
+            //    Console.WriteLine("No ha escrito ninguna frase.");
+            //}
+
+            //Console.ReadKey();
+
+            ///// =============================== /////////////
+            string frase;
+            HashSet<string> Diccionario = new HashSet<string>();
+
+            Console.WriteLine("Ingrese frase al alzar. Si ya no desea ingresar mas frases, no escriba nada y de ENTER.");
+            frase = Console.ReadLine();
+            Diccionario.Add(frase);
+
+            while (frase != "")
+            {
+                frase = Console.ReadLine();
+
+                Diccionario.Add(frase);
+
+                if (Diccionario.Contains(frase))
+                {
+                    Console.WriteLine("La frase ingresada, fue escrita anteriormente");
+                }
+                else
+                {
+                    Console.WriteLine("La frase ingresada, NO fue escrita anteriormente");
+                }
+
+            }
+
+            if (frase == "")
+            {
+                Console.WriteLine("No ha escrito ninguna frase.");
+            }
+
+            Console.ReadKey();
+        }
+    
+    }
+}
